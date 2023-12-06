2f44c4e3cfe963a52ba1f92e98f7c457f79bdd6b
SergioPUCLM <sergio.pozuelo4@alu.uclm.es>
2023-12-06 18:15:47 +0100

    Añadidas las sujerencias de la Issue Lanzar KeyError()

--- icedrive_directory/directory_manager.py
+++ icedrive_directory/directory_manager.py
@@ -1,6 +1,8 @@
 import os
 import json
 from typing import List
+import uuid as UD
+import hashlib
 
 class Directory:
     def __init__(self, name, parent=None):  # Create a directory
@@ -15,8 +17,11 @@ class Directory:
     def getChilds(self)-> List[str]:  # Return a list of childs
         return list(self.childs.keys())
 
-    def getChild(self, name):  # Return a Directory child object
-        return self.childs.get(name)
+    def getChild(self, name):
+        try:
+            return self.childs[name]
+        except KeyError:
+            raise ChildNotExists(name, path=self.getPath())
 
     def createChild(self, name):  # Create a child
         if name not in self.childs:  # Check if it already exists
@@ -35,8 +40,11 @@ class Directory:
     def getFiles(self)-> List[str]:  # Return a list of files
         return list(self.files.keys())
 
-    def getBlobId(self, filename):  # Find the bloblid of a file
-        return self.files.get(filename)
+    def getBlobId(self, filename):
+        try:
+            return self.files[filename]
+        except KeyError:
+            raise FileNotFound(filename)
 
     def linkFile(self, filename, blob_id):  # Add a new file
         if filename not in self.files:  # Check if the file exists
@@ -47,7 +55,7 @@ class Directory:
     def unlinkFile(self, filename):  # Delete a file
         if filename in self.files:  # Check if it exists
             del self.files[filename]  # Delete the file
-        else:  # If it doesn't exist, throw exception
+        else:  # If it doesn"t exist, throw exception
             raise FileNotFound(filename)
 
     def getPath(self):  # Get the path from root to the current dir
@@ -57,61 +65,67 @@ class Directory:
             return ""
 
 class DirectoryService:
-    def __init__(self, dataDir='./USRDIRS/'):  # Initialize the directory
+    def __init__(self, dataDir="./USRDIRS/"):
         self.dataDir = dataDir
-        os.makedirs(self.dataDir, exist_ok=True)  # Make the base directory if it doesn't exist
+        os.makedirs(self.dataDir, exist_ok=True)
 
-    def getUserFilePath(self, user):  # Get the path to the user json file
-        return os.path.join(self.dataDir, f'{user}.json')
+    def getUserFilePath(self, user):
+        uuid = self.genUUID(user)
+        return os.path.join(self.dataDir, f"{uuid}.json")
 
-    def getRoot(self, user):  # Return the root directory 
-        userFilePath = self.getUserFilePath(user)  # Find the path to the file
-        if os.path.exists(userFilePath):  # Check if the file exists
-            with open(userFilePath, 'r') as file:  # Open the file
+    def getRoot(self, user):
+        userFilePath = self.getUserFilePath(user)
+        if os.path.exists(userFilePath):
+            with open(userFilePath, "r") as file:
                 userData = json.load(file)
                 return self.loadDirectory(userData)
         else:
-            root = Directory(name='root')
+            root = Directory(name="root")
             self.saveDirectory(root, user)
             return root
 
-    def saveDirectory(self, directory, user):  # Save the directory into a user's json
+    def saveDirectory(self, directory, user):
         userFilePath = self.getUserFilePath(user)
         data = self.serializeDirectory(directory)
-        with open(userFilePath, 'w') as file:
+        with open(userFilePath, "w") as file:
             json.dump(data, file)
 
+    def genUUID(self, user):
+        namespace = UD.UUID("6ba7b810-9dad-11d1-80b4-00c04fd430c8")
+        uuid = UD.uuid5(namespace, user)
+        return str(uuid)
+
     def loadDirectory(self, data):  # Create a directory object from the json file
-        directory = Directory(name=data['name'])  # Create the directory
-        for childName, childData in data.get('childs', {}).items():  # Add the childs
+        directory = Directory(name=data["name"])  # Create the directory
+        for childName, childData in data.get("childs", {}).items():  # Add the childs
             child = self.loadDirectory(childData)
             directory.childs[childName] = child
             child.parent = directory
-        directory.files = data.get('files', {})
+        directory.files = data.get("files", {})
         return directory
 
     def serializeDirectory(self, directory):  # Convert the structure to json data
-        data = {'name': directory.name}
+        data = {"name": directory.name}
         if directory.childs:  # Check for child directories
-            data['childs'] = {name: self.serializeDirectory(child) 
+            data["childs"] = {name: self.serializeDirectory(child) 
                 for name, child in directory.childs.items()}
         if directory.files:  # Check for files
-            data['files'] = directory.files
+            data["files"] = directory.files
         return data
 
 # exceptions:
 class ChildAlreadyExists(Exception):
     def __init__(self, childName, path):
-        super().__init__(f"Child directory '{childName}' already exists in path: {path}")
+        super().__init__(f"Child directory \"{childName}\" already exists in path: {path}")
 
 class ChildNotExists(Exception):
     def __init__(self, childName, path):
-        super().__init__(f"Child directory '{childName}' does not exist in path: {path}")
+        super().__init__(f"Child directory \"{childName}\" does not exist in path: {path}")
 
 class FileAlreadyExists(Exception):
     def __init__(self, filename):
-        super().__init__(f"File '{filename}' already exists in the directory")
+        super().__init__(f"File \"{filename}\" already exists in the directory")
 
 class FileNotFound(Exception):
     def __init__(self, filename):
-        super().__init__(f"File '{filename}' not found in the directory")
+        super().__init__(f"File \"{filename}\" not found in the directory")
